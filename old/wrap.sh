3d6f66ea465eee96389d5a0cbac980fc79e9a3ea
Joe Talerico <jtaleric@redhat.com>
2015-10-26 15:03:17 -0400

    Removed old

--- old/wrap.sh
+++
@@ -1,7 +0,0 @@
-test="/var/lib/pbench/osp6-sec_group-enabled_iptables-vxlan-guest_1500-uperf-2"
-
-start-tools --dir=${test}
-./Scale-OSP-MultiNetwork-Perf.sh 0
-stop-tools --dir=${test}
-postprocess-tools --dir=${test}
-move-results --dir=${test}
